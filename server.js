160f864d5cc25c3399e4205f37a8dffea8169c67
Ryan Chase <ryan@chasery.com>
2020-12-21 08:35:30 -0800

    Prepare for first deployment!

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const POKEDEX = require("./pokedex.json");
 
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "dev";
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-
-  console.log("validate bearer token middleware");
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unautorized request" });
@@ -72,8 +71,16 @@ function handleGetPokemon(req, res) {
 
 app.get("/pokemon", handleGetPokemon);
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {});
